dcd4a879f6df8e14a4801e8944dc562e5dd19c2b
Kishan Mishra <tkishanmkm@gmail.com>
2026-02-04 03:12:30 +0000

    when trying to add wallet address and qr this issue as in image

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:766653104190:web:260f782fceabe29765d16f",
   "apiKey": "AIzaSyC74nnN0_Rh3jLX9HB3CSCwEpAPIMaBtXQ",
   "authDomain": "studio-426151189-64dac.firebaseapp.com",
+  "storageBucket": "studio-426151189-64dac.appspot.com",
   "measurementId": "",
   "messagingSenderId": "766653104190"
 };
