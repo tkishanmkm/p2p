a7ec89da7ab2c5332521bda4ae8d4eb44d600ad3
Kishan Mishra <tkishanmkm@gmail.com>
2026-02-05 12:38:18 +0000

    now for creating ad this is needed for another currency so add 183 curre

--- src/lib/currencies.ts
+++ src/lib/currencies.ts
@@ -1,1 +1,19 @@
-export const currencies = [];
+export const currencies = [
+    "USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "HKD", "NZD",
+    "SEK", "KRW", "SGD", "NOK", "MXN", "INR", "RUB", "ZAR", "TRY", "BRL",
+    "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AWG", "AZN", "BAM",
+    "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BSD", "BTN",
+    "BWP", "BYN", "BZD", "CDF", "CLP", "COP", "CRC", "CUP", "CVE", "CZK",
+    "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "FJD", "FKP", "FOK",
+    "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HNL", "HRK",
+    "HTG", "HUF", "IDR", "ILS", "IMP", "IQD", "IRR", "ISK", "JEP", "JMD",
+    "JOD", "KES", "KGS", "KHR", "KID", "KMF", "KWD", "KYD", "KZT", "LAK",
+    "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK",
+    "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MYR", "MZN", "NAD", "NGN",
+    "NIO", "NPR", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG",
+    "QAR", "RON", "RSD", "RWF", "SAR", "SBD", "SCR", "SDG", "SHP", "SLL",
+    "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND",
+    "TOP", "TTD", "TWD", "TZS", "UAH", "UGX", "UYU", "UZS", "VES", "VND",
+    "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZMW", "ZWL",
+    "GGP", "JEP", "IMP", "TVD", "FOK", "CKD", "KID", "PND"
+];
