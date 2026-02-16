7c515e4a5979d53a8684f903c6bce1c81a18af78
Kishan Mishra <tkishanmkm@gmail.com>
2026-02-16 20:28:19 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Firebase

--- src/lib/firebase-functions.ts
+++ src/lib/firebase-functions.ts
@@ -1,13 +1,14 @@
 
 import { getFunctions } from 'firebase/functions';
-import { getApp } from 'firebase/app';
+import { initializeFirebase } from '@/firebase';
 
 // Get a connection to the Cloud Functions emulator
 // if the FIRESTORE_EMULATOR_HOST environment variable is set
 const isEmulator = process.env.NEXT_PUBLIC_FUNCTIONS_EMULATOR_HOST;
 
 function getFirebaseFunctions() {
-  const functions = getFunctions(getApp());
+  const { firebaseApp } = initializeFirebase();
+  const functions = getFunctions(firebaseApp);
   if (isEmulator) {
     // connectFunctionsEmulator(functions, 'localhost', 5001);
   }
